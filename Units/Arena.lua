25947c4ac7609b4ae6cf398bee229f9b6defc19c
jdtoppin <6392799+jdtoppin@users.noreply.github.com>
2026-03-24 17:54:07 -0600

    Add Units/Boss.lua + Arena.lua: boss1-5 and arena1-5 frame definitions

---
+++ Units/Arena.lua
@@ -0,0 +1,37 @@
+local addonName, Framed = ...
+local F = Framed
+local oUF = F.oUF
+
+F.Units = F.Units or {}
+F.Units.Arena = {}
+
+-- ============================================================
+-- Style
+-- ============================================================
+
+local function Style(self, unit)
+	local config = F.StyleBuilder.GetConfig('arena')
+	F.StyleBuilder.Apply(self, unit, config)
+
+	if(F.Elements.CrowdControl) then
+		F.Elements.CrowdControl.Setup(self, { iconSize = 20 })
+	end
+end
+
+-- ============================================================
+-- Spawn
+-- ============================================================
+
+function F.Units.Arena.Spawn()
+	oUF:RegisterStyle('FramedArena', Style)
+	oUF:SetActiveStyle('FramedArena')
+
+	local frames = {}
+	for i = 1, 5 do
+		local arena = oUF:Spawn('arena' .. i, 'FramedArenaFrame' .. i)
+		arena:SetPoint('TOPRIGHT', UIParent, 'TOPRIGHT', -20, -200 - (i - 1) * 50)
+		frames[i] = arena
+	end
+
+	F.Units.Arena.frames = frames
+end
---
+++ Units/Boss.lua
@@ -0,0 +1,33 @@
+local addonName, Framed = ...
+local F = Framed
+local oUF = F.oUF
+
+F.Units = F.Units or {}
+F.Units.Boss = {}
+
+-- ============================================================
+-- Style
+-- ============================================================
+
+local function Style(self, unit)
+	local config = F.StyleBuilder.GetConfig('boss')
+	F.StyleBuilder.Apply(self, unit, config)
+end
+
+-- ============================================================
+-- Spawn
+-- ============================================================
+
+function F.Units.Boss.Spawn()
+	oUF:RegisterStyle('FramedBoss', Style)
+	oUF:SetActiveStyle('FramedBoss')
+
+	local frames = {}
+	for i = 1, 5 do
+		local boss = oUF:Spawn('boss' .. i, 'FramedBossFrame' .. i)
+		boss:SetPoint('TOPRIGHT', UIParent, 'TOPRIGHT', -20, -200 - (i - 1) * 50)
+		frames[i] = boss
+	end
+
+	F.Units.Boss.frames = frames
+end
